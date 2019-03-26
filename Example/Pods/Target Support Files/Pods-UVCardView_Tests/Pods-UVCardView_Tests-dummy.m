8641fd2b38adb2d31a31ee621338e7424a65df20
Umesh <umeshk.verma@webspiders.com>
2019-03-26 15:43:47 +0530

    Initial Commit

---
+++ Example/Pods/Target Support Files/Pods-UVCardView_Tests/Pods-UVCardView_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UVCardView_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_UVCardView_Tests
+@end
---
+++ Example/Pods/Target Support Files/UVCardView/UVCardView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_UVCardView : NSObject
+@end
+@implementation PodsDummy_UVCardView
+@end
